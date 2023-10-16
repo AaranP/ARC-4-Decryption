dcd3b31e658bb5d639220471ef9d57f6c3f7cb76
IdaraN <107487482+IdaraN@users.noreply.github.com>
2023-10-16 13:07:59 -0700

    Update arc4.sv

--- task3/arc4.sv
+++ task3/arc4.sv
@@ -6,11 +6,22 @@ module arc4(input logic clk, input logic rst_n,
 
     // your code here
 
-    s_mem s( /* connect ports */ );
-    init i( /* connect ports */ );
-    ksa k( /* connect ports */ );
-    prga p( /* connect ports */ );
+    wire smadd, swen;
+    wire [7:0] srdata, swdata;
+    wire rdyksa, rdyprga;
+    wire enksa, enprga;
 
-    // your code here
+    s_mem s(.address(smadd), .clock(clk), .data(swdata), .wren(swen), .q(srdata));
+    init i(.clk(clk), .rst_n(rst_n),.en(en), .rdy(rdy), .addr(smadd), .wrdata(swdata), .wren(swen));
+
+    ksa k( /* connect ports */ );
+    prga p(.clk(clk), .rst_n(rst_n), .en(enprga), .rdy(rdyprga),
+            .key(key), .s_addr(smadd), .s_rddata(srdata), .s_wrdata(swdata), .s_wren(swen),
+            .ct_addr(ct_adder), .ct_rddata(ct_rddata), .pt_addr(pt_addr),
+	    .pt_rddata(pt_rddata), .pt_wrdata(pt_wrdata), .pt_wren(pt_wren));
+
+
+
+    // your code here=
 
 endmodule: arc4
