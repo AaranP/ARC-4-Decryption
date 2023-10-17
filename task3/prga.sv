26001e10a451f8fd388d8972cb4b0629525156f3
IdaraN <107487482+IdaraN@users.noreply.github.com>
2023-10-17 14:59:12 -0700

    Update prga.sv

--- task3/prga.sv
+++ task3/prga.sv
@@ -18,7 +18,7 @@ module prga(input logic clk, input logic rst_n,
 
 logic [2:0] current_state;
 integer k;
-logic [7:0] length, i, j, count, data1, data2, value, value2, value3;
+	logic [7:0] length, i, j, k, count, data1, data2, value, value2, value3;
     // your code here
 
     always_ff@(posedge clk or negedge rst_n) begin
@@ -32,25 +32,29 @@ logic [7:0] length, i, j, count, data1, data2, value, value2, value3;
             case(current_state)
             
                 `reset : begin  
-                            i <= 1'b0;
-                            j <= 1'b0;
+                            i <= 8'd0;
+                            j <= 8'd0;
+		            k <= 8'd0;
                             count <= 1'b0;  
                             {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b1, 8'd0, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
                             current_state <= `start;
                          end
                 `start : begin
+			if( k < 8'd2) begin
                          length <= ct_rddata - 8'd1;
-			            {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, 8'd0, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};  
-                         if (en) begin 
+			 {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, 8'd0, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};  
+			end else begin
+			   k <= 8'd0;
+			   if (en) begin 
                            current_state <= `state1;
-                           rdy <= 1'b1;
-                         end else begin  
+                           rdy <= 1'b0;
+                       	   end else begin  
                             current_state <= `start;
-                         end
+                           end
                          end
                 `state1 : begin
                           if (count <= length) begin
-                             i <= (i + 8'b1) % 256;
+				  i <= (i + 8'd1) % 256;
                              {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b0, i, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
                               current_state <= `state2;
                           end else begin
@@ -60,31 +64,55 @@ logic [7:0] length, i, j, count, data1, data2, value, value2, value3;
                           end
                 
                 `state2 : begin
+			if (k < 8'd2) begin
+				current_state <= `state2;
+				k <= k + 8'd1;
+			end else begin
                           j <= (j + s_rddata) % 256;
                           data1 <= s_rddata;
                           {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b0, j, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
                           current_state <= `state3;
-                          end
+			  k <= 8'd0;
+			end
+			end
                 
                 `state3 : begin 
+			if (k< 8'd2) begin
+				current_state <= `state3;
+				k <= k + 8'd1;
+			end else begin
                           data2 <= s_rddata;
                           {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b0, (data1 + data2) % 256, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
                           current_state <= `state4;
-                          end
+			  k <= 8'd0;
+			end  
+			end
                 
                 `state4 : begin 
+			if( k < 8'd2) begin
+				current_state <= `state4;
+				k <= k + 8'd1;
+			end else begin
                           value <= s_rddata;
-                          {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count, count+8'd1, 8'd0, 1'b0};
+			  {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count + 8'd1, count, 8'd0, 1'b0};
                           current_state <= `state5;
+			   k <= 8'd0;
                           end
+			 end
                 `state5 : begin 
+			if (k < 8'd2) begin
+				current_state <= `state5;
+				k <= k + 8'd1;
+			end else begin
                           value2 <= ct_rddata;
-			            {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count, count, value2 ^ value, 1'b1};
+			  {rdy, s_  addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count+8'd1, count, value2 ^ value, 1'b1};
                           current_state <= `state6;
+			  k <= 8'd0;
                           end     
+			end
                 `state6 : begin
-			            {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count, count, value3, 1'b0};
-                          count <= count + 1;
+			{rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count, count, value2 ^ value, 1'b0};
+                          count <= count + 8'd1;
                           current_state <= `state1;
                           end
                 `done : begin
