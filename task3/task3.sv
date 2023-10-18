1a37101ba619d67622fb531274787e9eb4fe2c35
AaranP <66931430+AaranP@users.noreply.github.com>
2023-10-17 19:06:19 -0700

    instaitated arc4, pt and ct memory.

--- task3/task3.sv
+++ task3/task3.sv
@@ -5,10 +5,15 @@ module task3(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
 
     // your code here
 
-    ct_mem ct( /* connect ports */ );
-    pt_mem pt( /* connect ports */ );
-    arc4 a4( /* connect ports */ );
+    ct_mem ct(.address(ct_addr), .clock(CLOCK_50), .q(ct_rddata));
+
+    pt_mem pt(.address(pt_addr), .clock(CLOCK_50), .data(pt_wrdata), .wren(pt_wren), .q(pt_rddata));
+
+    arc4 a4(.clk(CLOCK_50), .rst_n(1), .en(en), .rdy(rdy), .key(key), 
+		.ct_addr(ct_addr), .ct_rddata(ct_rddata), .pt_addr(pt_addr), 
+		.pt_rddata(pt_rddata), .pt_wrdata(pt_wrdata), .pt_wren(pt_wren));
 
     // your code here
+    
 
 endmodule: task3
