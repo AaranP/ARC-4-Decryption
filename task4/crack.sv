27b8507c3a72b3f11dc2b160f0ab97c213fad9ba
IdaraN <107487482+IdaraN@users.noreply.github.com>
2023-10-18 23:26:38 -0700

    Update crack.sv
    accidentallly removed

--- task4/crack.sv
+++ task4/crack.sv
@@ -1,8 +1,13 @@
-`define start 3'd1
-`define state1 3'd2
-`define state2 3'd3
-`define state3 3'd4
-`define statea 3'd5
+`define reset 3'd1
+`define start 3'd2
+`define read_word 3'd3
+`define key_check 3'd4
+`define key_process 3'd5
+`define key_isvalid 3'd6
+`define key_notvalid 3'd7
+`define done 3'd8
+
+
 module crack(input logic clk, input logic rst_n,
              input logic en, output logic rdy,
              output logic [23:0] key, output logic key_valid,
@@ -10,75 +15,101 @@ module crack(input logic clk, input logic rst_n,
 
     // your code here
 
-        logic [7:0] pt_addr, pt_wrdata, pt_rddata, pause;
-        logic wren, arc4en, arcrdy, arcrst_n, pt_wren, k;
-        logic [3:0] current_state, next_state;
     // this memory must have the length-prefixed plaintext if key_valid
-        pt_mem pt(.address(pt_addr), .clock(clk), .data(pt_wrdata), .wren(pt_wren), .q(pt_rddata));
+            logic current_state, en_arc4, rdy_arc4;
+            logic[7:0] pt_addr, pt_wrdata, pt_wren, pt_rddata, count_pt;
 
-        arc4 a4(.clk(clk), .rst_n(arcrst_n),
-            .en(arc4en), .rdy(arcrdy), .key(key), .ct_addr(ct_addr), .ct_rddata(ct_rddata),
-            .pt_addr(pt_addr), .pt_rddata(pt_rddata), .pt_wrdata(pt_wrdata), .pt_wren(pt_wren));
+            logic[7:0] pt_arc4addr, pt_arc4wrdata, pt_arc4wren, pt_arc4rddata, ct_arc4addr;
 
+            pt_mem pt(.address(pt_addr), .clock(CLOCK_50), .data(pt_wrdata), .wren(pt_wren), .q(pt_rddata));
+
+            arc4 a4(.clk(clk), .rst_n(rst_n), .en(en_arc4), .rdy(rdy_arc4), .key(key), .ct_addr(ct_arc4addr), .ct_rddata(ct_rddata), .pt_addr(pt_arc4addr), 
+                        .pt_rddata(pt_arc4rddata), .pt_wrdata(pt_arc4wrdata), .pt_wren(pt_arc4wren));
     // your code here
-        always_ff@(posedge clk or negedge rst_n) begin
 
-                if(~rst_n) begin
-                        {rdy, key, key_valid, arcrst_n} <= {1'b1, 24'd0, 1'b0, 8'd0, 1'b0};
-                        k <= 1'b0;
-                        current_state <= `start;
-                end else begin
-                        case(current_state)
-                        
-                        `start : begin  
-                                {rdy, key, key_valid, arcrst_n} <= {1'b1, 24'd0, 1'b0, 1'b1};
-                                if (arcrdy && en) begin
-                                        arcrst_n <= 1'b1;
-                                        arc4en <= 1'b1;
-                                        rdy <= 1'b0;
-                                        current_state <= `state2;
-                                end else begin
-                                        current_state <= `start;
-                                end
-                                end
-                        `state1 : {current_state, arcrst_n, arc4en} <= {`state2, 1'b1, 1'b0};
-			`statea : {current_state, arcrst_n, arc4en} <= {`state2, 1'b1, 1'b1};
-			`state2 : begin
-                                arc4en <= 1'b0;
-                                if (~arcrdy) begin
-                                        if(pt_wren) begin
-                                                if((pt_wrdata < 8'h20 || pt_wrdata > 8'h7E) && pt_addr != 8'd0) begin
-                                                        {key, key_valid, arcrst_n, arc4en} <= {key + 24'd1, 1'b0, 1'b0, 1'b1};
-                                                        current_state <= `statea;
-                                                end else begin
-                                                        current_state <= `state2;
-                                                end
-                                        end else begin
-                                                current_state <= `state2;
-                                        end
-                                end else if (arcrdy == 1'b1 && key == 24'd255) begin
-                                        key_valid <= 1'b0;
-                                        current_state <= `state3;
-                                end else begin
-                                        key_valid <= 1'b1;
-                                        current_state <= `state3;
-                                end 
-			        end    
-                        `state3 : begin
-                                rdy <= 1'b1;
-                                current_state <= `state3;
-                                end
-                        default : current_state <= `state3;
-                        endcase
+            always@(posedge clk, negedge rest_n) begin
+                if(~reset_n) begin
+                    {pt_addr, pt_wrdata} <= {0,0};
+                    {pt_wren, pt_rddata} <= {0,0};
+                    rdy <= 0; 
+                     current_state <= `start
                 end
-        end
+                else begin
+                    case(current_state)
+                    `start: begin
+                            if(en) begin
+                                {pt_addr, pt_wrdata} <= {0,0};
+                                {pt_wren, pt_rddata, ct_addr} <= {0,0,0};
+                                rdy <= 0;
+                                en_arc4 <= 1;
+                                current_state <= `read_word;
+                            end
+                            else begin
+                                current_state <= `start;
+                            end
+                                
+                        
+                    end
+                    `read_word: begin
+                                if(rdy_arc4) begin
+                                    en_arc4 <= 0;
+                                    {pt_addr, pt_wrdata} <= {pt_arc4addr, pt_arc4wrdata};
+                                    {pt_wren, pt_rddata, ct_addr} <= {pt_arc4wren, pt_arc4rddata, ct_arc4addr};
+                                    rdy <= 0;
+                                    current_state <=  `check_word;
 
-always@(*) begin
+                                end
+                                else begin
+                                    current_state <= `read_word;
+                                end
+                    end
+                    `key_check: begin
+                                if((pt_rddata >= 'h20) && (pt_rddata <= 'h7E)) begin
+                                    if(count_pt == (ct_rddata - 1'b1)) begin
+                                            current_state <= `key_isvalid;                                          
+                                    end
+                                    else begin
+                                            current_state <= `read_word;
+                                            count_pt <= count_pt + 1'b1;
+                                end
+                                else begin 
+                                    current_state <= `key_process;
+                                    count_pt <= 0;
 
-case(current_state) 
+                                end
 
-`start: begin
-	
+                    end
 
+                    end
+
+                    `key_process: begin
+                                if(key < 24'b1) begin
+                                    key <= key + 1'b1;
+                                    current_state <= `read_word;
+                                end
+                                else begin
+
+                                    current_state <= `key_notvalid;
+                                end
+                    end
+
+                    `key_isvalid: begin
+                                    key_valid <= 1;
+                                    rdy <= 1;
+                    end
+
+                    `key_notvalid: begin
+                                    key_valid <=0;
+                                    rdy<= 1; 
+                                    current_state <= `start; 
+                    end
+                    
+                    default: current_state <= `start;
+                   
+                    endcase
+                end
+
+            end
 
 endmodule: crack
+                      
