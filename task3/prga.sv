bfd2fd0d5b7577e2fa2d98ee6054fc86120257ec
IdaraN <107487482+IdaraN@users.noreply.github.com>
2023-10-18 21:56:54 -0700

    Update prga.sv

--- task3/prga.sv
+++ task3/prga.sv
@@ -8,8 +8,9 @@
 `define state6      4'd8
 `define done        4'd9
 `define state7     4'd10 
-`define statea     4'd11
-`define stateb     4'd12
+`define state8     4'd11
+`define state9     4'd12
+`define donea 4'd13
 
 module prga(input logic clk, input logic rst_n,
             input logic en, output logic rdy,
@@ -18,133 +19,141 @@ module prga(input logic clk, input logic rst_n,
             output logic [7:0] ct_addr, input logic [7:0] ct_rddata,
             output logic [7:0] pt_addr, input logic [7:0] pt_rddata, output logic [7:0] pt_wrdata, output logic pt_wren);
 
-logic [3:0] current_state;
-	logic [7:0] length, i, j, k, count, data, data1, data2, value, value2, value3;
+    logic [3:0] current_state;
+	logic [7:0] length, i, j, k, si, sj, padk, ctk, pause;
     // your code here
 
     always_ff@(posedge clk or negedge rst_n) begin
         if(~rst_n) begin
-            current_state <= `reset;
-            i <= 1'b0;
-            j <= 1'b0;
-            count <= 1'b0;
-	    {data, data1, data2, value, value2, value3} <= {6 *{8'd0}};
+            current_state <= `start;
+            i <= 8'd0;
+            j <= 8'd0;
+            k <= 8'd0;
+	        {length, i, j, k, si, sj, padk, ctk, pause} <= {9 *{8'd0}};
             {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b1, 8'd0, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
+      
         end else begin
+        
             case(current_state)
-            
-                `reset : begin  
-                            i <= 8'd0;
-                            j <= 8'd0;
-		            k <= 8'd0;
-                            count <= 1'b0;  
-                            {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b1, 8'd0, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
-			    if (en) begin 
-                           current_state <= `start;
-                           rdy <= 1'b0;
-                       	   end else begin  
-                            current_state <= `reset;
-			    
-                           end             
-                         end
-                `start : begin
-			if( k < 8'd2) begin
-                         length <= ct_rddata;
-			 {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, 8'd0, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};  
-			 k <= k + 8'd1;
-		        current_state <= `start;
-			end else begin
-			   k <= 8'd0;
-			   current_state <= `state1;
-                         end
-			 end
-                `state1 : begin
-                          if (count < length) begin
-				i <= (i + 8'd1) % 256;
-                             {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b0, i, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
-                              current_state <= `state2;
-				k <= 8'd0;
-                          end else begin
-                            {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, 8'd0, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};  
-                            current_state <= `state7;
-                          end
-                          end
-                
-                `state2 : begin
-			if (k < 8'd2) begin
-				current_state <= `state2;
-				k <= k + 8'd1;
-                          	data1 = s_rddata;
-			end else begin
-                          j <= (j + data1) % 256;
-                          {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b0, j, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
-                          current_state <= `state3;
-			  k <= 8'd0;
-			end
-			end  
-                `state3 : begin 
-			if (k< 8'd2) begin
-				current_state <= `state3;
-				data2 = s_rddata;
-				k <= k + 8'd1;
-			end else begin
-                          
-                          {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b0, i, data2, 1'b1, 8'd0, 8'd0, 8'd0, 1'b0};
-                          //{rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b0, (data1 + data2) % 256, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
-                          current_state <= `statea;
-			  k <= 8'd0;
-			end  
-			end
-                `statea : begin
-			  {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b0, j, data1, 1'b1, 8'd0, 8'd0, 8'd0, 1'b0};
-                         current_state <= `stateb;
-			end
-		`stateb : begin
-			  {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b0, (data1 + data2) % 256, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
-                         current_state <= `state4;
-			end
-                `state4 : begin 
-			if( k < 8'd2) begin
-				current_state <= `state4;
-				value = s_rddata;
-				k <= k + 8'd1;
-			end else begin
-			  {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count + 8'd1, count, 8'd0, 1'b0};
-                          current_state <= `state5;
-			   k <= 8'd0;
-                          end
-			 end
-                `state5 : begin 
-			if (k < 8'd3) begin
-				current_state <= `state5;
-				value2 <= ct_rddata;
-				k <= k + 8'd1;
-			end else begin
-			  {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count+8'd1, count+8'd1, value2 ^ value, 1'b1};
-                          current_state <= `state6;
-			  k <= 8'd0;
-                          end     
-			end
-                `state6 : begin
-			{rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count, count, value2 ^ value, 1'b0};
-                          count <= count + 8'd1;
-                          current_state <= `state1;
-                          end
-		`state7 : begin
-			  {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {1'b0, s_addr, 8'd0, 1'b0, count, 8'd0, length, 1'b1};
-			  current_state <= `done;
-			  end
-                `done : begin
-                        {rdy, s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {1'b1, 8'd0, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
+
+            `start : begin 
+                     if (en) begin
+                        rdy <= 1'b0;
+                        current_state <= `state1;
+                        {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {8'd0, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
+                     end else begin
+                        current_state <= `start;
+                     end
+                    end
+
+            `state1 : begin // read and record length from ct
+                     if (pause < 8'd2) begin
+                        pause <= pause + 8'd1;
+                        current_state <= `state1;
+                     end else begin
+                        pause <= 8'd0;
+                        length <= ct_rddata;
+                        current_state <= `state2;
+                     end 
+                    end
+
+            `state2 : begin // initialize i
+                      {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ctk, 8'd0};
+                      if (k < (length + 8'd1)) begin
+                        current_state <= `state3;
+                        i <= (i+8'd1) % 256;
+                        
+                      end else begin
                         current_state <= `done;
+                      end
+                     end
+
+            `state3 : begin // rread from s[i]
+                      {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {i, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
+                       if (pause < 8'd2) begin
+                            current_state <= `state3;
+                            {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ctk, 8'd1 + pause};
+                       end else begin
+                            j <= ((j + s_rddata) % 256);
+                            {length, i, k, si, sj, padk, ctk, pause} <= {length, i, k, s_rddata, sj, padk, ctk, 8'd0};
+                            current_state <= `state4;
+                       end
                         end
 
-                
-                default : current_state <= `done;
+            `state4 : begin 
+                        {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {j, 8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
+                       if (pause < 8'd2) begin
+                            current_state <= `state4;
+                            {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ctk, pause + 8'd1};
+                       
+                       end else begin
+                            {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, s_rddata, padk, ctk, 8'd0};
+                            {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {j, si, 1'b1, 8'd0, 8'd0, 8'd0, 1'b0};
+                            current_state <= `state5;
+                       end
+                        end
+
+            `state5 : begin
+                      {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ctk, 8'd0};
+                      {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {i, sj, 1'b1, 8'd0, 8'd0, 8'd0, 1'b0};
+                       current_state <= `state6;
+                    end
+            
+            `state6 : begin
+                      s_addr <= ((si + sj) % 256);
+                      {s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {8'd0, 1'b0, 8'd0, 8'd0, 8'd0, 1'b0};
+                      if (pause < 8'd2) begin
+                          current_state <= `state6;
+                          {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ctk, pause + 8'd1};
+                      end else begin
+                          {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, s_rddata, ctk, 8'd0};
+                          {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {k, 8'd0, 1'b0, k + 8'd1, 8'd0, 8'd0, 1'b0};
+                           current_state <= `state7;
+                      end
+                    end
+            
+            `state7 : begin 
+                      {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} = {k, 8'd0, 1'b0, k + 8'd1, 8'd0, 8'd0, 1'b0};
+                        if (pause < 8'd2) begin
+                          current_state <= `state7;
+                          {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ctk, pause + 8'd1};
+                      end else begin
+                          {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ct_rddata, 8'd0};
+                           current_state <= `state8;
+                      end
+                    end
+            
+            `state8 : begin
+                      {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ctk, 8'd0};
+                      {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wren} <= {8'd0, 8'd0, 1'b0, 8'd0, k + 8'd1, 1'b1};
+                      pt_wrdata <= padk ^ ctk;
+                      current_state <= `state9;
+                    end
+
+            `state9 : begin
+                      {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {8'd0, 8'd0, 1'b0, 8'd0, k, pt_wrdata , 1'b0};
+                      {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k + 8'd1, si, sj, padk, ctk, 8'd0};
+                      current_state <= `state2;
+                    end
+
+            `done : begin
+                    {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ctk, 8'd0};
+                    {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {8'd0, 8'd0, 1'b0, 8'd0, 8'd0, length , 1'b1};
+                    current_state <= `donea;
+                    end
+	    `donea : begin
+		     {length, i, j, k, si, sj, padk, ctk, pause} <= {length, i, j, k, si, sj, padk, ctk, 8'd0};
+                     {s_addr, s_wrdata, s_wren, ct_addr, pt_addr, pt_wrdata, pt_wren} <= {8'd0, 8'd0, 1'b0, 8'd0, 8'd0, length , 1'b0};
+                     current_state <= `donea;
+		     end
+            default : current_state <= `donea;
             endcase
         end
-    end 
+    end
+                      
+
                                
                 
 
 endmodule: prga
+
