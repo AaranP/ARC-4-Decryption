489dcd9ef4bc88e6c58b9fa05350f829add9ddf6
AaranP <66931430+AaranP@users.noreply.github.com>
2023-10-20 17:49:24 -0700

    change state logic tb

--- task3/tb_syn_prga.sv
+++ task3/tb_syn_prga.sv
@@ -1,3 +1,17 @@
+`define reset       4'd1 //length of message is read
+`define start       4'd2
+`define state1      4'd3
+`define state2      4'd4
+`define state3      4'd5
+`define state4      4'd6 
+`define state5      4'd7
+`define state6      4'd8
+`define done        4'd9
+`define state7     4'd10 
+`define state8     4'd11
+`define state9     4'd12
+`define donea 4'd13
+
 module tb_syn_prga();
 
 // Your testbench goes here.
@@ -17,12 +31,11 @@ logic pt_wren;
 	    forever #5 clk <= ~clk;
         end
 
-        /*task printvalues;
+        task printvalues;
                 $display("rdy:%b, wren: %b, addr: %h, ct_addr: %h, ptaddr:%h, pt_wrdata: %h", 
                             rdy, wren, addr,ct_addr, pt_addr, pt_wrdata);
 
         endtask
-        */
 
           initial begin
                 rst_n = 1;
@@ -30,13 +43,29 @@ logic pt_wren;
                 key = 24'h000000;
                 ct_rddata = 8'b0011001;
                 #10;
-                assert(dut.length == 8'b0011001)
-                else $error("message length is not read properly");
+                assert(dut.current_state == `start)
+                else $error("state not in start mode");
+                printvalues;
+
                 #2500;
-                assert(dut.k == 8'b0011001)
-                else $error("k length is not read properly");
+                assert(dut.current_state == `donea)
+                else $error("enable is still on");
+                printvalues;
+
+                #2500;
+                assert(dut.current_state == `done)
+                else $error("state is not in done");
+                printvalues; 
                 #50;
+
+                rst_n=0;
+                assert(dut.current_state == `reset && rdy ==1)
+                else $error("state is not reset and ready is not set to 1 ");
+
+
                 $stop;
+
+
        
                
         end
